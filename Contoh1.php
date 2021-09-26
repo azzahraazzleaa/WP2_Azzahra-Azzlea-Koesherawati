190055e1c49cb338190fed24b223c56989d7a91e
azzahraazzleaa <90686416+azzahraazzleaa@users.noreply.github.com>
2021-09-26 20:15:48 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>" Hai Perkenalkan </h1>";
+        echo "Nama Saya Azzahra Azzlea
+        Saya tinggal di Cikarang
+        Saya suka belajar bahasa asing";
+    }
+}
